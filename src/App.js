f8918e0df84aadc07eed5eea42d9d65c9f57e037
EhsanAliKhan <64222974+EhsanAliKhan@users.noreply.github.com>
2020-08-09 20:39:19 +0500

    App,Child,Parents are updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState} from 'react';
+import Parent from './Parent.js';
 import './App.css';
 
 function App() {
+  let [number, setNumber] =useState(10)
   return (
     <div className="App">
-      <h1>Welcome to session 05</h1>
+      <h1>Welcome to App.JS </h1>
+      <Parent num={number}></Parent>
+      <button onClick = {()=>{setNumber(++number)}}>Update number</button>
     </div>
   );
 }
